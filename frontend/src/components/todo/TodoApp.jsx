cefd960d72cb3899558e9e07dd2cbb0f3a43e1c3
somratcste <somratcste@gmail.com>
2021-03-11 00:25:28 +0600

    task: update state on login

--- frontend/src/components/todo/TodoApp.jsx
+++ frontend/src/components/todo/TodoApp.jsx
@@ -11,11 +11,26 @@ class TodoApp extends Component {
 }
 
 class LoginComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: 'nazmul',
+            password: ''
+        }
+    }
+
+    handleChange = event => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+    }
+
     render() {
         return (
             <>
-                User Name: <input type="text" name="username" />
-                Password: <input type="text" name="password" />
+                User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
+                Password: <input type="text" name="password" value={this.state.password} onChange={this.handleChange} />
                 <button>Login</button>
             </>
         )
