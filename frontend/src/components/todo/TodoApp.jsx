23be355f0cccd14bb80583385ecf4be5b77c565e
somratcste <somratcste@gmail.com>
2021-03-11 00:52:52 +0600

    task: hardcoded authentication

--- frontend/src/components/todo/TodoApp.jsx
+++ frontend/src/components/todo/TodoApp.jsx
@@ -16,7 +16,9 @@ class LoginComponent extends Component {
         super(props);
         this.state = {
             username: 'nazmul',
-            password: ''
+            password: '',
+            hasLoginFailed: false,
+            showSuccessMessage: false
         }
     }
 
@@ -26,15 +28,47 @@ class LoginComponent extends Component {
         })
     }
 
+    loginClicked = event => {
+        // nazmul, dummy
+        if (this.state.username === 'nazmul' && this.state.password === 'dummy') {
+            this.setState({
+                showSuccessMessage: true,
+                hasLoginFailed: false
+            })
+        } else {
+            this.setState({
+                hasLoginFailed: true,
+                showSuccessMessage: false
+            })
+
+        }
+    }
+
     render() {
         return (
             <>
+                <ShowInvalidCredentials hasLoginFailed={this.state.hasLoginFailed} />
+                <ShowLoginSuccessMessage showSuccessMessage={this.state.showSuccessMessage} />
                 User Name: <input type="text" name="username" value={this.state.username} onChange={this.handleChange} />
-                Password: <input type="text" name="password" value={this.state.password} onChange={this.handleChange} />
-                <button>Login</button>
+                Password: <input type="password" name="password" value={this.state.password} onChange={this.handleChange} />
+                <button onClick={this.loginClicked}>Login</button>
             </>
         )
     }
+}
+
+function ShowInvalidCredentials(props) {
+    if (props.hasLoginFailed) {
+        return <div>Invalid Credentials</div>
+    }
+    return null;
+}
+
+function ShowLoginSuccessMessage(props) {
+    if (props.showSuccessMessage) {
+        return <div>Login Successful</div>
+    }
+    return null;
 }
 
 export default TodoApp
