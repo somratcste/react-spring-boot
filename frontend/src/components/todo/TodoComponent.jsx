760becbf711bfdf512496f03bf5d3bd6137d1b4a
somratcste <somratcste@gmail.com>
2021-03-28 11:21:16 +0600

    task: add form validation

--- frontend/src/components/todo/TodoComponent.jsx
+++ frontend/src/components/todo/TodoComponent.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import moment from 'moment'
-import {Field, Form, Formik} from "formik";
+import {ErrorMessage, Field, Form, Formik} from "formik";
 
 class TodoComponent extends Component {
 
@@ -17,6 +17,21 @@ class TodoComponent extends Component {
         console.log(value)
     }
 
+    validate(values) {
+        let errors = {};
+        if (!values.description) {
+            errors.description = "Enter a Description."
+        } else if (values.description.length < 5) {
+            errors.description = "Enter at least 5 character in description."
+        }
+
+        if (!moment(values.targetDate).isValid()) {
+            errors.targetDate = "Enter a valid target date."
+        }
+
+        return errors;
+    }
+
     render() {
         let {description, targetDate} = this.state
 
@@ -27,10 +42,15 @@ class TodoComponent extends Component {
                     <Formik
                         initialValues={{description,targetDate}}
                         onSubmit={this.onSubmit}
+                        validateOnChange={false}
+                        validateOnBlur={false}
+                        validate={this.validate}
                     >
                         {
                             (props) => (
                                 <Form>
+                                    <ErrorMessage name="description" component="div" className="alert alert-warning" />
+                                    <ErrorMessage name="targetDate" component="div" className="alert alert-warning" />
                                     <fieldset className="form-group">
                                         <label>Description</label>
                                         <Field className="form-control" type="text" name="description" />
